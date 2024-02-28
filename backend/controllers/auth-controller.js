2ddf3d4cb3834dceb13be2ca6516f1ff5abb2aa1
Abhijeet Kumbhar <98834949+Abhiikumbhar@users.noreply.github.com>
2024-02-28 09:43:48 +0530

    /rooms

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,83 +1,87 @@
-const otpService = require("../services/otp-service");
-const hashService = require("../services/hash-service");
-const userModel = require("../services/user-service");
-const tokenService = require("../services/token-service");
+const otpService = require('../services/otp-service');
+const hashService = require('../services/hash-service');
+const userService = require('../services/user-service');
+const tokenService = require('../services/token-service');
 const UserDto = require('../dtos/user-dto');
 
-class AuthController{
-    async sendOtp(req,res){
-        const {phone} = req.body;
-        if(!phone){
-            res.status(400).json({message: 'PHONE filed required'});
+class AuthController {
+    async sendOtp(req, res) {
+        const { phone } = req.body;
+        if (!phone) {
+            res.status(400).json({ message: 'Phone field is required!' });
         }
 
         const otp = await otpService.generateOtp();
-        //hash the otp
-        const ttl = 1000 * 60 * 2;          //ttl- time to leave
+        // const otp = 7777;
+
+        const ttl = 1000 * 60 * 2; // 2 min
         const expires = Date.now() + ttl;
         const data = `${phone}.${otp}.${expires}`;
         const hash = hashService.hashOtp(data);
 
-        try{
-            // await otpService.sendBySms(phone, otp);   //this line commented only for sending otp on dummy number after testing we should uncomment this line
-            return res.json({
+        // send OTP
+        try {
+            // await otpService.sendBySms(phone, otp);
+            res.json({
                 hash: `${hash}.${expires}`,
                 phone,
                 otp
             });
-        }catch(err){
+        } catch (err) {
             console.log(err);
-            return res.status(500).json({message:'OTP Send Failed.!'});
+            res.status(500).json({ message: 'message sending failed' });
         }
     }
 
-    async verifyOtp(req,res ){
-        const { phone, otp ,hash }= req.body;
-        if(!phone || !otp || !hash){
-            return res.status(400).json({message:"All Fields Are Required.!"});
+    async verifyOtp(req, res) {
+        const { otp, hash, phone } = req.body;
+        if (!otp || !hash || !phone) {
+            res.status(400).json({ message: 'All fields are required!' });
         }
-        const [hashedOtp, expires] = hash.split(".");
-        if(Date.now() > +expires){
-            res.status(400).json({message:"OTP expired"});
+
+        const [hashedOtp, expires] = hash.split('.');
+        if (Date.now() > +expires) {
+            res.status(400).json({ message: 'OTP expired!' });
         }
 
         const data = `${phone}.${otp}.${expires}`;
-        const isValid = otpService.verifyOtp(hashedOtp, data)
-
-        if(!isValid){
-            res.status(400).json({message:"Invalid OTP.!"});
+        const isValid = otpService.verifyOtp(hashedOtp, data);
+        if (!isValid) {
+            res.status(400).json({ message: 'Invalid OTP' });
         }
 
         let user;
         try {
-            user = await userModel.findUser({phone: phone});
-            if(!user){
-                user = await userModel.createUser({phone: phone});
+            user = await userService.findUser({ phone });
+            if (!user) {
+                user = await userService.createUser({ phone });
             }
         } catch (err) {
             console.log(err);
-            res.status(500).json({message:"DB Error.!"});
+            res.status(500).json({ message: 'Db error' });
         }
-        console.log(user);
 
-        const {accessToken, refreshToken} = tokenService.generateTokens({
+        const { accessToken, refreshToken } = tokenService.generateTokens({
             _id: user._id,
             activated: false,
         });
 
         await tokenService.storeRefreshToken(refreshToken, user._id);
+
         res.cookie('refreshToken', refreshToken, {
             maxAge: 1000 * 60 * 60 * 24 * 30,
             httpOnly: true,
         });
+
         res.cookie('accessToken', accessToken, {
             maxAge: 1000 * 60 * 60 * 24 * 30,
             httpOnly: true,
         });
 
         const userDto = new UserDto(user);
-        res.json({ user: userDto, auth: true});
+        res.json({ user: userDto, auth: true });
     }
+
     async refresh(req, res) {
         // get refresh token from cookie
         const { refreshToken: refreshTokenFromCookie } = req.cookies;
@@ -143,6 +147,5 @@ class AuthController{
         res.json({ user: null, auth: false });
     }
 }
-
 
 module.exports = new AuthController();
--- frontend/src/components/shared/Card/Card.jsx
+++ frontend/src/components/shared/Card/Card.jsx
@@ -4,22 +4,17 @@ import styles from './Card.module.css';
 const Card = ({ title, icon, children }) => {
     return (
         <div className={styles.card}>
-
             <div className={styles.headingWrapper}>
-
-                <img src={`/images/${icon}.png`} alt="logo" />
-
-                <h1 className={styles.heading}>{title}</h1>
-
+                {icon && <img src={`/images/${icon}.png`} alt="logo" />}
+                {title && <h1 className={styles.heading}>{title}</h1>}
             </div>
-
-            {children} 
-
+            {children}
         </div>
     );
 };
 
 export default Card;
+
 
 
 //on this page {childrean} - we can injecting this children in all the other pages and in this code we can passthe title , icon and childrenn as a props for getting the input data ... for passing content of card we use chidren object
